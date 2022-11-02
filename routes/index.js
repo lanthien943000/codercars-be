7593bfa6d8985bbed08daa5b243e35b5788aebbb
lanthien943000 <hongnhat24022000@gmail.com>
2022-11-02 19:51:39 +0700

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,31 @@ var router = express.Router();
 
 /* GET home page. */
 
+router.get("/", function (req, res, next) {
+  res.status(200).send("Welcome to CoderSchool!");
+});
+
+router.get("/template/:test", async (req, res, next) => {
+  const { test } = req.params;
+  try {
+    //turn on to test error handling
+    if (test === "error") {
+      throw new AppError(401, "Access denied", "Authentication Error");
+    } else {
+      sendResponse(
+        res,
+        200,
+        true,
+        { data: "template" },
+        null,
+        "template success"
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 const carRouter = require("./car.api.js");
 router.use("/cars", carRouter);
 
