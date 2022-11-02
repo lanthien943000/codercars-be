c772b15201d6620f57b4ad974b1be922792472f7
lanthien943000 <hongnhat24022000@gmail.com>
2022-11-02 19:15:45 +0700

    fix index.js

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.status(200).send("Welcome to CoderSchool!");
-});
 
 const carRouter = require("./car.api.js");
 router.use("/cars", carRouter);
